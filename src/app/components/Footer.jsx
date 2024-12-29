be16f40b109dc10948ec2aedacdee142c1784d50
Talal Mahmud <mahmudtalal2@gmail.com>
2024-12-29 07:02:10 +0600

    softmax id can changed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,8 +5,15 @@ const Footer = () => {
     <div className=" w-full bg-brand text-white py-8 ">
       <div className=" mx-auto w-full xl:w-[1100px] h-full px-4 xl:px-0 flex flex-col gap-4 ">
         <div className=" flex w-full flex-col gap-4 xl:flex-row justify-between ">
-          <div className=" w-[351px]  flex flex-col gap-4">
+          <div className=" w-[351px]  flex flex-col gap-1">
             <p className="text-[24px] xl:text-[36px] font-bold">OrbGenix</p>
+            <p className="text-[12px] xl:text-[14px]">
+              {" "}
+              OrbGenix conveys the idea of a company that is innovative,
+              intelligent, and capable of producing cutting-edge solutions on a
+              global scale. It suggests a focus on technology that is both
+              advanced and creative.
+            </p>
           </div>
           <div className=" w-[88px] flex flex-col gap-4">
             <p className=" text-[19px] font-semibold">Company</p>
